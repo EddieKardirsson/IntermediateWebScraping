d13cadde556638ab9d0843126af57f14d9919861
Edison Kardirsson <ed.kardirsson@hotmail.com>
2024-11-30 16:27:31 +0100

    Parse infobox

--- webpage_scraping.py
+++ webpage_scraping.py
@@ -8,6 +8,8 @@ def get_html(url, path):
     with open(path, 'w', encoding="utf-8") as f:
         f.write(response.text)
 
+print(' ')
+
 path = "./html_docs/bristlecone.html"
 url = "https://en.wikipedia.org/wiki/Bristlecone_pine"
 # get_html(url, path)
@@ -22,6 +24,28 @@ section_headings = soup.find_all('h2')
 # print(section_headings)
 
 section_headings = [h2.string for h2 in section_headings]
-print(section_headings)
-print(type(section_headings[0]))
-print(section_headings[0].parent)
+# print(section_headings)
+# print(type(section_headings[0]))
+# print(section_headings[0].parent)
+
+taxonomy = {}
+infobox = soup.find('table', attrs={'class': 'infobox biota'})
+# print(infobox)
+
+def taxonomy_filter(tag):
+    return ':' in tag.text and tag.name == 'td'
+
+filtered = infobox.find_all(taxonomy_filter)
+print(filtered)
+
+for tag in filtered:
+    sibling = tag.next_sibling.next_sibling
+
+    taxonomy[tag.text.strip().replace(':', '')] = sibling.text.strip()
+
+print(taxonomy)
+
+def another_taxonomy_filter(tag):
+    return tag.name == 'tr' and len(list(tag.children)) == 4
+
+print('Second method: ', infobox.find_all(another_taxonomy_filter))
