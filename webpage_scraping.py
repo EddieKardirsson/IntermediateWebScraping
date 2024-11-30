33d1eaa94f153e95d53b2bca7d7fad1faff82366
Edison Kardirsson <ed.kardirsson@hotmail.com>
2024-11-30 15:57:35 +0100

    Extract headings

--- webpage_scraping.py
+++ webpage_scraping.py
@@ -16,4 +16,12 @@ with open(path, 'r', encoding="utf-8") as f:
     html = f.read()
 
 soup = BeautifulSoup(html, 'html.parser')
-print(soup.title)
+# print(soup.title)
+
+section_headings = soup.find_all('h2')
+# print(section_headings)
+
+section_headings = [h2.string for h2 in section_headings]
+print(section_headings)
+print(type(section_headings[0]))
+print(section_headings[0].parent)
